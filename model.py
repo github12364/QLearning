79d28da390538de24d879686211f117bbe7d9901
Leo Winiecki <leodwiniecki@outlook.com>
2019-03-02 19:58:42 -0800

    keras convolution layer

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 import numpy as np
+import keras
 
 class Model:
     def __init__(self):
-        self.w1 = np.zeros(())
-        self.b1 = np.zeros(())
-        self.w2 = np.zeros(())
-        self.b2 = np.zeros(())
-        self.w3 = np.zeros(())
-        self.b3 = np.zeros(())
+        return
+        
+    def train():
+        keras.layers.Conv1D(filters, kernel_size, strides=1, padding='valid', data_format='channels_last', dilation_rate=1, activation=None,
+                             use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', 
+                            kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
+        
